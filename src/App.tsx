bb02ffc5a6c98f1e14d40c9bf75b78e439701d30
juanenguerra-cmd <juan.enguerra@gmail.com>
2026-02-04 05:33:57 -0500

    Adjust layout to fit dynamic viewport

--- src/App.tsx
+++ src/App.tsx
@@ -62,11 +62,11 @@ function AppContent() {
   };
 
   return (
-    <div className="flex h-screen w-full bg-background">
+    <div className="flex h-dvh w-full bg-background">
       <AppSidebar mobileOpen={mobileNavOpen} onMobileOpenChange={setMobileNavOpen} />
-      <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
+      <div className="flex-1 flex min-h-0 flex-col min-w-0 overflow-hidden">
         <AppHeader onMenuClick={() => setMobileNavOpen(true)} />
-        <main className="flex-1 overflow-auto p-4 lg:p-6">
+        <main className="flex-1 min-h-0 overflow-auto p-4 lg:p-6">
           <BackupReminderBanner />
           {renderPage()}
         </main>
